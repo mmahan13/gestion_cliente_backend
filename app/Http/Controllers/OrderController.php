e3f7045f6c93b7da860df0b02e498a8425ed1078
michaell.mahan <mmahan13@gmail.com>
2020-07-21 14:51:43 +0200

    create order header & lines

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,19 +8,40 @@ use App\LineOrder;
 
 class OrderController extends Controller
 {
+    public function getNumberOrder(){
+        return $number_order = HeaderOrder::max('id') + 1;
+    }
+
     public function get($id){
         $order = HeaderOrder::find($id);
         return $order;
     }
 
     public function getAllHeaderOrder(){
-        $orders =  HeaderOrder::all();
+        $orders =  HeaderOrder::orderBy('id','DESC')->get();;
         return $orders;
     }
 
-    public function create(Request $request){
-        $order = HeaderOrder::create($request->all());
-        return $order;
+    public function createHeaderOrder($header){
+        $header = HeaderOrder::create($header);
+        return $header;
+    }
+
+    public function createLinesOrder($lines, $id_header_orders){
+       
+        foreach($lines as $line) {
+            $lineOrder = new LineOrder;
+            $lineOrder->id_clients = $line['id_user'];
+            $lineOrder->id_header_orders = $id_header_orders;
+            $lineOrder->id_products = $line['id'];
+            $lineOrder->ref = $line['ref'];
+            $lineOrder->name  = $line['name'];
+            $lineOrder->quantity  = $line['quantity'];
+            $lineOrder->price  = $line['price'];
+            $lineOrder->total_lines = $line['total_lines'];
+            $lineOrder->save();
+        }
+        return $lineOrder;
     }
 
     public function update($id, Request $request){
@@ -39,5 +60,16 @@ class OrderController extends Controller
     public function getLines($id){
         $lines = LineOrder::where('id_header_orders', $id)->get();
         return $lines;
+    }
+
+    public function createOrder(Request $request){
+        $data = $request->all();
+        $numberorder = $this->getNumberOrder();
+        $data[0]['number_orders'] = isset($numberorder) ? $numberorder: 1;
+        $o['header'] = $this->createHeaderOrder($data[0]);
+        if(isset($o['header'])){
+           $o['lines'] = $this->createLinesOrder($data[1], $o['header']['id']);
+        }
+        return $o;
     }
 }
