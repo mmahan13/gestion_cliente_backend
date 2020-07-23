6ae83428ba54894d3f3ea8913ec9ddaf7dccbcfc
michaell.mahan <mmahan13@gmail.com>
2020-07-23 13:12:39 +0200

    add campos migrations

--- app/LineOrder.php
+++ app/LineOrder.php
@@ -8,6 +8,6 @@ class LineOrder extends Model
 {
     protected $table = 'gestion.lines_orders';
     protected $fillable = [
-        'id_clients', 'id_header_orders', 'id_products', 'name', 'quantity', 'price'
+        'id_clients', 'id_header_orders', 'id_products', 'name', 'quantity', 'price','total'
     ];
 }
